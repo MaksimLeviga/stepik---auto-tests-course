f4860472c4e44bf1217bd24d90a08c06e9161dc9
Maks <mleviga@firstgaming.com>
2020-04-10 19:03:58 +0300

    auto test 4

---
+++ 4.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    browser = webdriver.Chrome()
+    browser.get("http://suninjuly.github.io/math.html")
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    y = calc(x)
+    elements = browser.find_elements_by_id ("answer")
+    for element in elements:
+       element.send_keys(y)
+    option1 = browser.find_element_by_id("robotCheckbox")
+    option1.click()
+    option2 = browser.find_element_by_id("robotsRule")
+    option2.click()
+    button = browser.find_element_by_xpath("/html/body/div/form/button")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
