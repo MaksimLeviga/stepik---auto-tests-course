72662e1b8a63debc84894e338ad306f31a4e28cf
Maks <mleviga@firstgaming.com>
2020-04-10 19:03:50 +0300

    auto test 5

---
+++ 5.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    browser = webdriver.Chrome()
+    browser.get("http://suninjuly.github.io/get_attribute.html")
+    treasure_x = browser.find_element_by_id("treasure")
+    x_element = treasure_x.get_attribute("valuex")
+    x = x_element
+    y = calc(x)
+    elements = browser.find_elements_by_id ("answer")
+    for element in elements:
+       element.send_keys(y)
+    option1 = browser.find_element_by_id("robotCheckbox")
+    option1.click()
+    option2 = browser.find_element_by_id("robotsRule")
+    option2.click()
+    button = browser.find_element_by_xpath("/html/body/div/form/div/div/button")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(15)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
